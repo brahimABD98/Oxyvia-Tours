29f22d9574c193f7b01823f48d02d0e976f49fe1
brahimABD98 <47871510+brahimABD98@users.noreply.github.com>
2021-03-05 20:30:50 +0100

    1

---
+++ src/Form/ReclamationType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Reclamation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReclamationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('type')
+            ->add('nomClient')
+            ->add('EmailClient')
+            ->add('contenu')
+            ->add('date')
+            ->add('etat')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Reclamation::class,
+        ]);
+    }
+}
